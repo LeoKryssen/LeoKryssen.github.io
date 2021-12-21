7d08d5e169fc7f8c6467e199b90a113758277af8
LeoKryssen <shangshan3@outlook.com>
2021-12-21 16:20:59 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -95,7 +95,7 @@ $(function() {
     resize()
     ////////////////////////////////////////
 })
-$('.photos a').click(function(event){
+$('.container a').click(function(event){
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
         event.preventDefault();
     }
